66bd29d3a876efb413e84177192a9580878ddf7c
daniel <contacto@mindit.com.ar>
2019-10-14 16:32:59 -0300

    chequea que no se suban insumos con codigos duplicados

--- app/routesInsumos.js
+++ app/routesInsumos.js
@@ -72,10 +72,25 @@ module.exports = function (app,connection, passport) {
   app.post('/insert-insumos', bodyJson,checkConnection, function (req, res) {
 
       var arrayIns = [req.body.codigo, req.body.descripcion, req.body.unidad, req.body.minimo, 1, req.body.categoria, req.body.numero];
-      connection.query("INSERT INTO insumos (codigo, descripcion, unidad, minimo, activo, id_insumos_categorias, numero) VALUES (?)", [arrayIns], function (err, result) {
-        if (err) return res.json({ success: 0, error_msj: "ha ocurrido un error al intentar insertar un insumo", err });
-        res.json({ success: 1, result });
+      
+      connection.query("SELECT * FROM insumos WHERE id_insumos_categorias = ? AND numero = ?", [req.body.categoria, req.body.numero], function (err, result) {
+        if (err) return res.json({ success: 0, error_msj: "ha ocurrido un error al chequear duplicidad de insumos", err });
+        else{
+          if(result.length > 1) {
+            return res.json({ success: 0, error_msj: "No se puede guardar, código ya utilizado. Ingrese otro.", err });  
+          }
+          else {
+            connection.query("INSERT INTO insumos (codigo, descripcion, unidad, minimo, activo, id_insumos_categorias, numero) VALUES (?)", [arrayIns], function (err, result) {
+              if (err) return res.json({ success: 0, error_msj: "ha ocurrido un error al intentar insertar un insumo", err });
+              res.json({ success: 1, result });
+            })
+         }
+          
+        }
+        
       })
+      
+     
 
   });
 
